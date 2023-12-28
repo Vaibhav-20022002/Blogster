f41840990612f9020c69d95df610be2ea4cf304f
Vaibhav-20022002 <vibhu20022002@gmail.com>
2023-12-28 21:13:10 +0530

    Storing Blogs and Displaying as Card

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const userRouter = require("./routes/user");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/auth");
+const Blog = require("./models/blog");
 
 const app = express();
 
@@ -26,14 +28,18 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+	const allBlogs = await Blog.find({});
 	res.render("home", {
 		user: req.user,
+		blogs: allBlogs,
 	});
 });
 
-app.use("/user", userRouter);
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on: ${PORT}`);
